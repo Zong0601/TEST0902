2870f195040da6c9cb9147456ca76116abe0cf37
Zong <ricky11614@gmail.com>
2019-09-02 11:20:48 +0800

    TEST TEST TEST

---
+++ test0820/test0820/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace test0820
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Capture_But = new System.Windows.Forms.Button();
+            this.Capture_ImageBox = new Emgu.CV.UI.ImageBox();
+            this.Gray_ImageBox = new Emgu.CV.UI.ImageBox();
+            this.Binary_ImageBox = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Capture_ImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gray_ImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Binary_ImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Capture_But
+            // 
+            this.Capture_But.Font = new System.Drawing.Font("新細明體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Capture_But.Location = new System.Drawing.Point(31, 64);
+            this.Capture_But.Name = "Capture_But";
+            this.Capture_But.Size = new System.Drawing.Size(200, 48);
+            this.Capture_But.TabIndex = 0;
+            this.Capture_But.Text = "開啟鏡頭";
+            this.Capture_But.UseVisualStyleBackColor = true;
+            this.Capture_But.Click += new System.EventHandler(this.Capture_But_Click);
+            // 
+            // Capture_ImageBox
+            // 
+            this.Capture_ImageBox.Location = new System.Drawing.Point(12, 214);
+            this.Capture_ImageBox.Name = "Capture_ImageBox";
+            this.Capture_ImageBox.Size = new System.Drawing.Size(239, 208);
+            this.Capture_ImageBox.TabIndex = 2;
+            this.Capture_ImageBox.TabStop = false;
+            // 
+            // Gray_ImageBox
+            // 
+            this.Gray_ImageBox.Location = new System.Drawing.Point(287, 214);
+            this.Gray_ImageBox.Name = "Gray_ImageBox";
+            this.Gray_ImageBox.Size = new System.Drawing.Size(242, 208);
+            this.Gray_ImageBox.TabIndex = 3;
+            this.Gray_ImageBox.TabStop = false;
+            // 
+            // Binary_ImageBox
+            // 
+            this.Binary_ImageBox.Location = new System.Drawing.Point(563, 214);
+            this.Binary_ImageBox.Name = "Binary_ImageBox";
+            this.Binary_ImageBox.Size = new System.Drawing.Size(232, 208);
+            this.Binary_ImageBox.TabIndex = 4;
+            this.Binary_ImageBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(329, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 48);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "開啟路徑上的照片並且做一些功能";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Binary_ImageBox);
+            this.Controls.Add(this.Gray_ImageBox);
+            this.Controls.Add(this.Capture_ImageBox);
+            this.Controls.Add(this.Capture_But);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Capture_ImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gray_ImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Binary_ImageBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Capture_But;
+        private Emgu.CV.UI.ImageBox Capture_ImageBox;
+        private Emgu.CV.UI.ImageBox Gray_ImageBox;
+        private Emgu.CV.UI.ImageBox Binary_ImageBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ test0820/test0820/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+namespace test0820
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        //擷取攝影機的影像
+        private Capture capture;
+        //判斷攝影機鏡頭狀態
+        private bool captureinprocess;
+
+        /// <summary> 
+        /// 啟動攝影機與暫停攝影機
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+
+
+        private void Capture_But_Click(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                if (captureinprocess)
+                {
+                    Application.Idle -= new EventHandler(ProcessFrame);
+                    Capture_But.Text = "Start！";
+                }
+                else
+                {
+                    Application.Idle += new EventHandler(ProcessFrame);
+                    Capture_But.Text = "Stop！";
+                }
+                captureinprocess = !captureinprocess;
+            }
+            else
+            {
+                try
+                {
+                    capture = new Capture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+        }
+        /// <summary>
+        /// 讀取攝影機的影像畫面並且顯示在ImageBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="arg"></param>
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            //顯示擷取到的影像
+            Image<Bgr, Byte> ImageFrame = capture.QueryFrame().ToImage<Bgr, Byte>();
+            Capture_ImageBox.Image = ImageFrame;
+
+            //將讀取到的影像轉換成灰階 並顯示
+            Image<Gray, Byte> GrayFrame = ImageFrame.Convert<Gray, Byte>();
+            Gray_ImageBox.Image = GrayFrame;
+
+            Image<Gray, Byte> CannyFrame = ImageFrame.Canny(120, 255);
+            Binary_ImageBox.Image = CannyFrame;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //路徑設定
+            string imagePath = "C:\\Users\\user\\Desktop\\circle.gif"; 
+            Image<Bgr, Byte> original = new Image<Bgr, Byte>(imagePath);
+            //直接顯示
+            CvInvoke.Imshow("original", original);
+
+            //灰階處理
+            Image<Gray, Byte> Gray = original.Convert<Gray, Byte>();
+            CvInvoke.Imshow("Gray", Gray);
+
+            //水平Sobel
+            Image<Gray, Single> SobelS = Gray.Sobel(2, 0, 3);
+            CvInvoke.Imshow("SobelS", SobelS);
+            //垂直Sobel
+            Image<Gray, Single> SobelC = Gray.Sobel(0, 2, 3);
+            CvInvoke.Imshow("SobelC", SobelC);
+
+            //整合Sobel
+            Image<Gray, Single> SobelA = SobelC.Sobel(2, 0, 3);
+            CvInvoke.Imshow("SobelA", SobelA);
+
+            //霍夫找/畫圓
+            CircleF[] circles = CvInvoke.HoughCircles(Gray, HoughType.Gradient, 1, 50, 100, 12, 50, 70);
+            Image<Bgr, Byte> circleImage = original.Clone();
+            foreach (CircleF circle in circles)
+            {
+                circleImage.Draw(circle, new Bgr(Color.Red), 5);
+                Console.WriteLine("圓心座標 X: " + circle.Center.X.ToString() + "  Y: " + circle.Center.Y.ToString());
+            }
+            CvInvoke.Imshow("HoughCircles", circleImage);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+  
